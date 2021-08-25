4fc6bc1104c4410450cd30dba65742ef478b3477
Steve <la14b@yahoo.co.uk>
2021-08-25 06:19:24 +0000

    Add style to therules div and video

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,32 @@ const containerDiv = document.getElementsByClassName('container');
 const playButton = document.getElementById('submit-name');
 const gameBoardDiv = document.getElementById('game-board');
 const userInputDiv = document.getElementById('user-input');
+const rules = document.getElementById('rules');
+const rulesImage = document.getElementById('rules-image');
+
+/** This variable will hold the HTML for the game options page */
 
 let gameOptions = `
 <div id="game-options-page">
     <p id="message">Please enter your name above to play game.</p>
-    <p id="user-options-message">Please select a game</p>
-    <button class="game-button" id="short-game">First to 3</button>
-    <button class="game-button" id="medium-game">First to 5</button>
-    <button class="game-button" id="long-game">First to 10</button>
     <div id="rules-video">
         <a id="video" href="https://www.youtube.com/watch?v=fqlDc2VICZ0" target="_blank" rel="noopener" aria-label="View video of rules (opens a new tab)">Rules Video</a>
     </div>
     <div id="rules">How to play
         <img class="hide" id="rules-image" src="assets/images/rules.png" alt="rules image">
     </div>
-</div>`;
+</div>
+    <p id="user-options-message">Please select a game</p>
+    <button class="game-button" id="short-game">First to 3</button>
+    <button class="game-button" id="medium-game">First to 5</button>
+    <button class="game-button" id="long-game">First to 10</button>
+    `;
 
+/**
+ * This function will store the name. of the user
+ */
+
+function userInput() {
 playButton.addEventListener('click', function() {
    let userInputName = document.getElementById('fname').value;
         firstName = userInputName;
@@ -26,12 +36,23 @@ playButton.addEventListener('click', function() {
             document.getElementById('message').innerHTML = ('Please enter your name to play!');
         } else {
             gameBoardDiv.innerHTML = gameOptions; 
-            document.getElementById('message').innerHTML = ('Hey ' + firstName + ' are you ready to play? Click on either Rock, Paper, Scissors, Lizard or Spock to play');
+            document.getElementById('message').innerHTML = ('Hey ' + firstName + ' are you ready to play? Choose from the different game options below');
             userInputDiv.classList.add('hide');
         }
 })
+}
 
-/**
- * This function will store the name. of the user
+userInput();
+
+/** 
+ * This functions will show and hide the game rules and how to play. 
  */
 
+function showRules() {
+    rules.addEventListener('click', function() {
+    rulesImage.classList.toggle('hide');
+    console.log('im hidden');
+    });
+}
+
+showRules();
